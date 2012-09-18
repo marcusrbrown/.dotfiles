abf48653fefe5e021775701bcfd13020605db710
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-18 01:46:23 -0700

    Disable builtin virtualenv prompts.

--- bash/init.d/virtualenvwrapper.bash
+++ bash/init.d/virtualenvwrapper.bash
@@ -3,6 +3,7 @@
 # virtualenvwrapper initialization.
 
 export VIRTUALENV_DISTRIBUTE=1
+export VIRTUAL_ENV_DISABLE_PROMPT=1
 
 __garbage VENVWRAPPERSH
 VENVWRAPPERSH=$(type -P virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh | head -1)
