cab4f4edeb00962726d50cfd29d7ffe04b6bdb1e
Marcus R. Brown <mrbrown@precision-mojo.com>
2012-09-18 12:25:26 -0700

    Remove the virtualenv MSYS hack, it's already obsolete.

--- bash/init.d/virtualenvwrapper.bash
+++ bash/init.d/virtualenvwrapper.bash
@@ -11,16 +11,4 @@ VENVWRAPPERSH=$(type -P virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh
 if [[ -n "$VENVWRAPPERSH" && -z "$WORKON_HOME" ]]; then
   export WORKON_HOME="$HOME/.virtualenvs"
   source "$VENVWRAPPERSH"
-
-  # This is a hack for virtualenv on Windows under MSYS. The virtualenv activate code will only install
-  # `activate' (not the .bat) if it detects Cygwin by looking at OSTYPE. We set OSTYPE to "cygwin" when
-  # executing mkvirtualenv so that virtualenv works properly from a MSYS shell.
-  # "Copy function" trick taken from http://stackoverflow.com/a/1369211.
-  if [[ "$HOST_OS" = "mingw" ]]; then
-    eval "$(echo "orig_mkvirtualenv()"; declare -f mkvirtualenv | tail -n +2)"
-    #unset -f mkvirtualenv
-    function mkvirtualenv {
-      OSTYPE=cygwin orig_mkvirtualenv "$@"
-    }
-  fi
 fi
