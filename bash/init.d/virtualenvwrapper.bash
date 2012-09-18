93a437e18aed754f1849c526ac1fd0e94bae8d04
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-18 01:44:13 -0700

    Add a virtualenvwrapper bash init script.

---
+++ bash/init.d/virtualenvwrapper.bash
@@ -0,0 +1,13 @@
+#!bash
+#
+# virtualenvwrapper initialization.
+
+export VIRTUALENV_DISTRIBUTE=1
+
+__garbage VENVWRAPPERSH
+VENVWRAPPERSH=$(type -P virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh | head -1)
+
+if [[ -n "$VENVWRAPPERSH" && -z "$WORKON_HOME" ]]; then
+  export WORKON_HOME="$HOME/.virtualenvs"
+  source "$VENVWRAPPERSH"
+fi
