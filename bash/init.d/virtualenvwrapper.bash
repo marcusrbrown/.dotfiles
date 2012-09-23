617486b6849aa91ea15abf21bd1ee857d9806d9a
Marcus R. Brown <mrossbrown@gmail.com>
2012-09-23 12:47:03 -0700

    Set WORKON_HOME only if it hasn't already been set.
    If WORKON_HOME has been set, export it in a format compatible with the
    shell (translating Windows paths to the format used by MSYS).

--- bash/init.d/virtualenvwrapper.bash
+++ bash/init.d/virtualenvwrapper.bash
@@ -8,7 +8,12 @@ export VIRTUAL_ENV_DISABLE_PROMPT=1
 __garbage VENVWRAPPERSH
 VENVWRAPPERSH=$(type -P virtualenvwrapper.sh /usr/local/bin/virtualenvwrapper.sh | head -1)
 
-if [[ -n "$VENVWRAPPERSH" && -z "$WORKON_HOME" ]]; then
-  export WORKON_HOME="$HOME/.virtualenvs"
+if [[ -n "$VENVWRAPPERSH" ]]; then
+  if [[ -z "$WORKON_HOME" ]]; then
+    export WORKON_HOME="$HOME/.virtualenvs"
+  else
+    # Make sure the directory is in a format compatible with the shell.
+    export WORKON_HOME="$(cd "$WORKON_HOME" && pwd)"
+  fi
   source "$VENVWRAPPERSH"
 fi
