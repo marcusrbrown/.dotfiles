7dd294ce7ee10d40e89e670efb33984bac072a0f
Marcus R. Brown <mrbrown@precision-mojo.com>
2013-01-03 23:00:49 -0700

    Add bash completion for Rake.

---
+++ bash/completion.d/rake.bash
@@ -0,0 +1,20 @@
+#!bash
+#
+# Bash completion support for Rake, Ruby Make.
+#
+# Originally from https://github.com/ai/rake-completion.
+
+export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
+
+_rakecomplete() {
+    if [ -f Rakefile ]; then
+        recent=`ls -t .rake_tasks~ Rakefile **/*.rake 2> /dev/null | head -n 1`
+        if [[ $recent != '.rake_tasks~' ]]; then
+            rake --silent --prereqs | grep "rake" | cut -d " " -f 2 > .rake_tasks~
+        fi
+        COMPREPLY=($(compgen -W "`cat .rake_tasks~`" -- ${COMP_WORDS[COMP_CWORD]}))
+        return 0
+    fi
+}
+
+complete -o default -o nospace -F _rakecomplete rake
