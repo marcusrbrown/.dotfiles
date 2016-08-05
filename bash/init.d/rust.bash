737d13b795e4e408b564da547b2acec878a6de5f
Marcus R. Brown <me@igetgam.es>
2016-08-05 14:01:38 -0700

    Add RUST_SRC_PATH.

--- bash/init.d/rust.bash
+++ bash/init.d/rust.bash
@@ -2,4 +2,7 @@
 #
 # Rust setup.
 
-export PATH=$PATH:$HOME/.cargo/bin
+[ -d "$HOME/.cargo/bin" ] && export PATH="$PATH:$HOME/.cargo/bin"
+
+# For Racer.
+[ -d "$HOME/git/rust-lang/rust/src" ] && export RUST_SRC_PATH="$HOME/git/rust-lang/rust/src"
