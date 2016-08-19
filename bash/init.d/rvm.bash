b68a08c4400a8bd6070361f122e92252b6464a8b
Marcus R. Brown <me@igetgam.es>
2016-08-19 12:37:00 -0700

    Setup rvm.

---
+++ bash/init.d/rvm.bash
@@ -0,0 +1,7 @@
+#!bash
+#
+# rvm setup
+
+[[ -d "$HOME/.rvm/bin" ]] && export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
