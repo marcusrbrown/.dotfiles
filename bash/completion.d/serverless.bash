e7c55448e525c58eb359b156339af87c302d9d3e
Marcus R. Brown <me@igetgam.es>
2018-02-28 19:23:35 -0700

    Add completion scripts for yarn and serverless.

---
+++ bash/completion.d/serverless.bash
@@ -0,0 +1,12 @@
+#!bash
+#
+# Completions for the serverless npm package.
+
+# TODO: Use nvm or other means to locate the node package directory.
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.bash ] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.bash
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.bash ] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.bash
