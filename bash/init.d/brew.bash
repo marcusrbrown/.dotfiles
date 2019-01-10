ee3c8016bc1140f661d9de32106bd8020e96831a
Marcus R. Brown <me@igetgam.es>
2019-01-10 00:20:45 -0700

    Add more Homebrew environment variables.

--- bash/init.d/brew.bash
+++ bash/init.d/brew.bash
@@ -4,3 +4,9 @@
 
 # Disable 'anonymous' analytics
 export HOMEBREW_NO_ANALYTICS=1
+
+# Remove previously installed versions of insalled/upgraded formulae
+export HOMEBREW_INSTALL_CLEANUP=1
+
+# Do not permit redirects from HTTPS to HTTP
+export HOMEBREW_NO_INSECURE_REDIRECT=1
