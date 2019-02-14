eba732fddd943476968f7ecf65d6b4a8ddd02019
Marcus R. Brown <me@igetgam.es>
2019-02-14 11:41:04 -0700

    Prefer Homebrew's Python over the system Python.

--- bash/init.d/brew.bash
+++ bash/init.d/brew.bash
@@ -10,3 +10,7 @@ export HOMEBREW_INSTALL_CLEANUP=1
 
 # Do not permit redirects from HTTPS to HTTP
 export HOMEBREW_NO_INSECURE_REDIRECT=1
+
+# Point to Homebrew's Python. Note that this must be installed in the PATH
+# ahead of the system path to Python (usually /usr/bin/python).
+__set_path PATH "/usr/local/opt/python/libexec/bin"
