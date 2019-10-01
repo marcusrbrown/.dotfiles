d90f949b40dea740b160fc54f3775d2d3b62095f
Marcus R. Brown <me@igetgam.es>
2019-10-01 00:12:35 -0700

    Add support for loading recent bash-completion

--- bash/init.d/completion.bash
+++ bash/init.d/completion.bash
@@ -2,12 +2,17 @@
 #
 # Command completion
 
+if [ -z "${BASH_COMPLETION_COMPAT_DIR}" -a -d /usr/local/etc/bash_completion.d ]; then
+  export BASH_COMPLETION_COMPAT_DIR="/usr/local/etc/bash_completion.d"
+fi
+
 # Only source completion if the shell is interactive.
 if [ -n "$INTERACTIVE" ]; then
   bash=${BASH_VERSION%.*}; bmajor=${bash%.*}; bminor=${bash#*.}
-  if [ $bmajor -gt 1 -a -z "${BASH_COMPLETION_VERSINFO-}" ]; then
+  if [ $bmajor -gt 1 ]; then
     # Search for a bash_completion file to source.
-    for f in /usr/local/etc/bash_completion \
+    for f in /usr/local/etc/profile.d/bash_completion.sh \
+             /usr/local/etc/bash_completion \
              /usr/pkg/etc/bash_completion \
              /opt/local/etc/bash_completion \
              /usr/share/bash-completion/bash_completion \
