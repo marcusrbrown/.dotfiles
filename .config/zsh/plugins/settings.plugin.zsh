5a92c06fc27d30fedf3999502f7d49e06f002ad3
Marcus R. Brown <contact@marcusrbrown.com>
2024-04-25 01:23:37 -0700

    feat(sheldon): move inline plugins into local plugin scripts

---
+++ .config/zsh/plugins/settings.plugin.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+# shellcheck disable=SC1071
+
+setopt append_history         # append to the history file across all shells
+setopt inc_append_history     # write to the history file immediately, not when the shell exits
