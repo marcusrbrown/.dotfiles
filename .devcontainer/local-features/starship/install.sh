86b7d9ad51e2e465568ff1fe37cd7d6f3e7696b9
Marcus R. Brown <contact@marcusrbrown.com>
2024-08-21 01:34:20 -0700

    chore: add prerequisites as features

---
+++ .devcontainer/local-features/starship/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
