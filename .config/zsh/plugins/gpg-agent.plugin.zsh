38a66004d78c5cc6589120f6bf483ec5e67b3408
Marcus R. Brown <contact@marcusrbrown.com>
2024-08-27 04:02:26 -0700

    chore: move `gpg-agent` config/code to the ZSH plugin

--- .config/zsh/plugins/gpg-agent.plugin.zsh
+++ .config/zsh/plugins/gpg-agent.plugin.zsh
@@ -1,10 +1,20 @@
 #!/usr/bin/env zsh
-# shellcheck disable=SC1071
+
+export GPG_TTY=$TTY
 
 # Ensure the passphrase prompt is shown in the correct tty
+# See https://www.gnupg.org/documentation/manuals/gnupg/Agent-Options.html#option-_002d_002denable_002dssh_002dsupport
 function _gpg-agent_update-tty_preexec {
   gpg-connect-agent updatestartuptty /bye &>/dev/null
 }
 
 autoload -U add-zsh-hook
 add-zsh-hook preexec _gpg-agent_update-tty_preexec
+
+# Set up gpg-agent to work with ssh
+if command_exists gpgconf && [[ $(gpgconf --list-options gpg-agent 2>/dev/null | awk -F: '$1=="enable-ssh-support" {print $10}') = 1 ]]; then
+  unset SSH_AGENT_PID
+  if [[ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]]; then
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+  fi
+fi
