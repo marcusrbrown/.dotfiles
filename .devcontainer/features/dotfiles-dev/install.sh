1c6586f473a15880b366082b0323c49a93c34ed6
Marcus R. Brown <git@mrbro.dev>
2025-05-11 00:48:27 -0700

    chore(devcontainer): improve post-create script for dotfiles setup
    - Simplified script generation for 'post-create.sh' using echo.
    - Updated condition to check for GH_TOKEN using '-z' for better readability.
    - Added checks for existing dotfiles and .gitconfig file.
    - Install tools (e.g., `node`, `poetry`, etc.) using mise

--- .devcontainer/features/dotfiles-dev/install.sh
+++ .devcontainer/features/dotfiles-dev/install.sh
@@ -44,14 +44,11 @@ fi
 # --- Generate a 'post-create.sh' script to be executed by the 'postCreateCommand' lifecycle hook
 POST_CREATE_SCRIPT_PATH="${DOTFILES_DEV_PATH}/post-create.sh"
 
-tee "$POST_CREATE_SCRIPT_PATH" > /dev/null << EOF
-#!/bin/bash
-set -e
-EOF
+echo -e "#!/bin/bash\nset -e\n" > "$POST_CREATE_SCRIPT_PATH"
 
+# Clone the bare repo and check out the main branch
 tee -a "$POST_CREATE_SCRIPT_PATH" > /dev/null << 'EOF'
-
-if [ "$GH_TOKEN" == "" ]; then
+if [ -z "$GH_TOKEN" ]; then
     unset GH_TOKEN
     echo "The GH_TOKEN environment variable is not set. The script will attempt to clone the dotfiles repo without it."
 fi
@@ -60,6 +57,7 @@ GIT_DIR="${GIT_DIR:-${HOME}/.dotfiles}"
 GIT_WORK_TREE="${GIT_WORK_TREE:-${HOME}}"
 export GIT_DIR GIT_WORK_TREE
 
+# Check if the dotfiles bare repo is already cloned
 if ! git ls-files -- "$GIT_DIR" >/dev/null 2>&1; then
     echo "Cloning dotfiles into bare repo ‘${GIT_DIR}’..."
     if [ -n "$GH_TOKEN" ] && type gh >/dev/null 2>&1; then
@@ -69,6 +67,7 @@ if ! git ls-files -- "$GIT_DIR" >/dev/null 2>&1; then
     fi
 fi
 
+# Check if the .gitconfig file exists; if not, checkout the main branch to install the dotfiles
 if git ls-files -- "$GIT_DIR" >/dev/null 2>&1; then
     if [ ! -s "${GIT_DIR}/.gitconfig" ]; then
         echo "Checking out dotfiles bare repo into ‘${GIT_WORK_TREE}’..."
@@ -76,6 +75,19 @@ if git ls-files -- "$GIT_DIR" >/dev/null 2>&1; then
         git config --local include.path .gitconfig
     fi
 fi
+
+EOF
+
+# If mise is installed, install tools
+tee -a "$POST_CREATE_SCRIPT_PATH" > /dev/null << 'EOF'
+# Install tools using mise:
+# - Disable parallel installs.
+# - Show verbose output.
+# - Always answer "yes" to prompts.
+if type mise > /dev/null 2>&1; then
+    mise install --jobs=1 --verbose --yes 2>&1
+fi
+
 EOF
 
 chmod 0755 "$POST_CREATE_SCRIPT_PATH"
